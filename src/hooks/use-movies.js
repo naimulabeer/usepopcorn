843caadf9a4dd96b48c6b6577663cc4df789b560
naimulabeer <naimul.h.abeer@gmail.com>
2023-12-09 03:16:09 +0600

    bugs updated

--- src/hooks/use-movies.js
+++ src/hooks/use-movies.js
@@ -15,7 +15,7 @@ export default function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
